4c2546225912b02313427ec2233574353edea99e
Sanchayan Das <45485694+sanchayancse@users.noreply.github.com>
2020-09-15 00:46:50 +0530

    register component done

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,77 +1,89 @@
-import React, {useState} from "react";
-import { Button, Form, Grid, Header, Icon, Segment } from "semantic-ui-react";
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Icon,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
 function Register() {
-
-    let user={
-        username: '',
-        email: '',
-        password: '',
-        confirmpassword: ''
-    }
-
-    let errors = [];
-    const [userState, setuserState] = useState(user);
-    const [errorState, seterrorState] = useState(errors);
-  const handleInput = (e) => {
-      let target = e.target;
-      setuserState((currentState)=>{
-          let currentuser = {...currentState};
-          currentuser[target.name] = target.value;
-          return currentuser;
-
-      })
-
+  let user = {
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
   };
 
-const checkForm = () =>{
-    if(isFormEmpty()){
-        seterrorState((error) => error.concat({message: "please fill all the fields"}))
-        return false;
-    }
-    else if(checkPassword()){
-        return false;
+  let errors = [];
+  const [userState, setuserState] = useState(user);
+  const [errorState, seterrorState] = useState(errors);
+  const handleInput = (e) => {
+    let target = e.target;
+    setuserState((currentState) => {
+      let currentuser = { ...currentState };
+      currentuser[target.name] = target.value;
+      return currentuser;
+    });
+  };
+
+  const checkForm = () => {
+    if (isFormEmpty()) {
+      seterrorState((error) =>
+        error.concat({ message: "please fill all the fields" })
+      );
+      return false;
+    } else if (!checkPassword()) {
+      seterrorState((error) =>
+        error.concat({ message: "Given password not valid" })
+      );
+      return false;
     }
     return true;
+  };
 
-}
+  const isFormEmpty = () => {
+    return (
+      !userState.username.length ||
+      !userState.password.length ||
+      !userState.confirmpassword.length ||
+      !userState.email.length
+    );
+  };
 
-const checkPassword = () =>{
-    if(userState.password.length < 8){
-        return false;
-    }
-    else if(userState.password !== userState.confirmpassword){
-        return false;
+  const checkPassword = () => {
+    if (userState.password.length < 8) {
+      return false;
+    } else if (userState.password !== userState.confirmpassword) {
+      return false;
     }
     return true;
+  };
 
-}
+  const formatErrors = () => {
+    return errorState.map((error, index) => <p key={index}>{error.message}</p>);
+  };
 
-const isFormEmpty = () => {
-    return !userState.username.length || 
-    !userState.password.length ||
-     !userState.confirmpassword.length ||
-     !userState.email.length;
-}
 
-const onSubmit =(e) =>{
-    if(checkForm()){
-
-    }else{
-
+  const onSubmit = (e) => {
+      seterrorState(() =>[]);
+    if (checkForm()) {
+    } else {
     }
+  };
 
-}
+  
 
   return (
     <Grid verticalAlign="middle" textAlign="center">
-      <Grid.Column style={{maxWidth:'500px'}}>
-          <Header as="h2">
-              <Icon name="slack"/>
-                Register
-              
-          </Header>
-        <Form onSubmit={onSubmit} >
+      <Grid.Column style={{ maxWidth: "500px" }}>
+        <Header as="h2">
+          <Icon name="slack" />
+          Register
+        </Header>
+        <Form onSubmit={onSubmit}>
           <Segment stacked>
             <Form.Input
               name="username"
@@ -84,9 +96,9 @@ const onSubmit =(e) =>{
             />
 
             <Form.Input
-              name="emai;"
+              name="email"
               value={userState.email}
-              icon="email"
+              icon="mail"
               iconPosition="left"
               onChange={handleInput}
               type="email"
@@ -116,6 +128,12 @@ const onSubmit =(e) =>{
 
           <Button>Submit</Button>
         </Form>
+        {errorState.length > 0 && (
+          <Message error>
+            <h3>Errors</h3>
+            {formatErrors()}
+          </Message>
+        )}
       </Grid.Column>
     </Grid>
   );
