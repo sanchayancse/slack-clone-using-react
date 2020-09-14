e4722406d7a48e72e04ccf8dc07571603e5bfbdc
Sanchayan Das <45485694+sanchayancse@users.noreply.github.com>
2020-09-14 23:07:06 +0530

    First commit

---
+++ src/config/fire.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/database';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDaYnGz5qJSXKF435odqPuY3yKY4amMyro",
+    authDomain: "slack-clone-752a4.firebaseapp.com",
+    databaseURL: "https://slack-clone-752a4.firebaseio.com",
+    projectId: "slack-clone-752a4",
+    storageBucket: "slack-clone-752a4.appspot.com",
+    messagingSenderId: "1084432378597",
+    appId: "1:1084432378597:web:0ca71539d6518e4bbf820c",
+    measurementId: "G-6CWX3X4JMM"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+  export default fire;
